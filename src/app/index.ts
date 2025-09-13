78daf72bb3fc6487907db2888abdedb720dd9717
renato <neo_rlm@hotmail.com>
2025-09-12 21:15:28 -0300

    logServer

--- src/app/index.ts
+++ src/app/index.ts
@@ -14,7 +14,7 @@ export async function buildServer(
   config: FastifyServerOptions = {}
 ): Promise<FastifyInstance> {
   const server = Fastify({
-    logger: false,
+    logger: true,
     trustProxy: true,
   });
 
