8c0796c486833e58a69262e2d506eec8b1e0d8e5
Renato <neo_rlm@hotmail.com>
2025-09-24 14:42:24 -0300

    CORS

--- src/lib/fastifyPlugins/fastifyModule.ts
+++ src/lib/fastifyPlugins/fastifyModule.ts
@@ -35,7 +35,8 @@ const fastifyModule = fp(async (fastify) => {
   const allowedOrigins = [
     "http://127.0.0.1:5500",
     "http://localhost:5173",
-    "*",
+    "https://web.panelapps.site",
+    "https://renatobh83.github.io",
   ];
 
   await fastify.register(cors, {
