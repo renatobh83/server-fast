10464ef377c0988f6616ee47c6d10444423c4f32
Renato <neo_rlm@hotmail.com>
2025-11-10 11:38:42 -0300

    udpateBot

--- src/services/ChatFlowServices/Helpers/TemplateMessage/Telegram/index.ts
+++ src/services/ChatFlowServices/Helpers/TemplateMessage/Telegram/index.ts
@@ -1,60 +1,64 @@
-import { ListMessageChamadosTbot, ListMessageEmpresaTbot, SemChamadoTbot, SemEmpresaAssociadoTbot } from "../../optionsListMensagens";
+import {
+  ListMessageChamadosTbot,
+  ListMessageEmpresaTbot,
+  SemChamadoTbot,
+  SemEmpresaAssociadoTbot,
+} from "../../optionsListMensagens";
 
 export const TemplateEmpresaSelecaoTbot = (contatoEmpresa: any[]) => {
-
-
-  const rowsListMessage = contatoEmpresa.map(empresa => ([
+  const rowsListMessage = contatoEmpresa.map((empresa) => [
     {
       callback_data: `Empresa_${empresa.id}`,
       text: `#️⃣ ${empresa.name}`,
-    }
-  ]));
+    },
+  ]);
 
+  rowsListMessage.push([
+    {
+      callback_data: "3",
+      text: "❌ Finalizar Atendimento",
+    },
+  ]);
 
-  rowsListMessage.push([{
-    callback_data: "3",
-    text: "❌ Finalizar Atendimento"
-  }]);
+  const options = ListMessageEmpresaTbot(rowsListMessage);
 
-  const options = ListMessageEmpresaTbot(rowsListMessage)
-
-  return options
-}
+  return options;
+};
 export const TemplateChamadoSelecaoTbot = (tickets: any[]) => {
-  const rows = [[{
-    callback_data: "suporte",
-    text: "🏷️ Falar no suporte",
-  }]]
-  rows.push([{
-    callback_data: "voltar",
-    text: "⬅️ Voltar",
-  }])
+  const rows = [
+    [
+      {
+        callback_data: "suporte",
+        text: "🏷️ Falar no suporte",
+      },
+    ],
+  ];
+
   const finalizarAtendimento = {
     callback_data: "3",
     text: "❌ Finalizar atendimento",
-  }
+  };
 
-  rows.push([finalizarAtendimento])
+  rows.push([finalizarAtendimento]);
   if (tickets.length === 0) {
-    const options = SemChamadoTbot(rows)
-    return options
-
+    const options = SemChamadoTbot(rows);
+    return options;
   }
 
-  const rowsListMessage = tickets.map(chamado => ([
+  const rowsListMessage = tickets.map((chamado) => [
     {
       callback_data: `Chamado_${chamado.id}`,
       text: `#️⃣ ${chamado.id} - ${chamado.assunto} - ${chamado.status} `,
-    }
-  ]));
+    },
+  ]);
 
+  rowsListMessage.push([
+    {
+      callback_data: "3",
+      text: "❌ Finalizar Atendimento",
+    },
+  ]);
 
-  rowsListMessage.push([{
-    callback_data: "3",
-    text: "❌ Finalizar Atendimento"
-  }]);
-
-
-  const options = ListMessageChamadosTbot(rowsListMessage)
-  return options
-}
+  const options = ListMessageChamadosTbot(rowsListMessage);
+  return options;
+};
